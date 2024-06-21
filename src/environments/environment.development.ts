56eb1160f0db65b2d94336c38ee59eef4f9eb43a
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-06-21 14:14:39 -0400

    Runway client

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     api: "https://us-east-1.aws.data.mongodb-api.com/app/application-0-emmnfdy/endpoint",
     audience: "https://us-east-1.aws.data.mongodb-api.com/app/application-0-emmnfdy/endpoint",
-    clientId: "RchOI5LaEUxkoJi9wHNIozA9i4yHK39h",
+    clientId: "vJhx6eIz7A31D6esBUTDpQGlDHNmMaCe",
     domain: "dev-qx1mijjm2d37sd8u.us.auth0.com",
 };
