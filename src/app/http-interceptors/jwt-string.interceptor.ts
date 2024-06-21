5c069a2645c9ad62a80b7add349e36bcbafa7758
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-06-21 15:52:25 -0400

    TODOs

--- src/app/http-interceptors/jwt-string.interceptor.ts
+++ src/app/http-interceptors/jwt-string.interceptor.ts
@@ -1,10 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
+// TODO : Waiting for Mongo to address aud array marshal issue
 export const jwtStringInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = req.headers.get("Authorization");
 
   if (req.url.includes(environment.api) && authToken) {
+    // TODO : This breaks CORS
     // req = req.clone({ setHeaders: { jwtTokenString: authToken } });
   }
 
