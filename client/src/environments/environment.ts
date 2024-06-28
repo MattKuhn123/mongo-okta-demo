55987d48d28ba2de37c7f85ba5a0422e2f56e2f8
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-06-28 15:49:23 -0400

    client environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    api: "http://localhost:3000",
+    api: "https://mongo-okta-demo-6wn1.vercel.app",
     audience: "https://dev-qx1mijjm2d37sd8u.us.auth0.com/api/v2/",
     clientId: "vJhx6eIz7A31D6esBUTDpQGlDHNmMaCe",
     domain: "dev-qx1mijjm2d37sd8u.us.auth0.com",
